a424b4433c21d553497335a5758ab51d74fe50ab
Valery Filatov <valiklfiatoff@yandex.ru>
2023-01-12 17:10:07 +0300

    Token
    Added place to enter token

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
-TOKEN = '5917862155:AAEcuEqSc4eDY99sIr37oplHjxsC3GjWrCw'
+#Enter the token here
+TOKEN = ':)'
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
 
-MSG = 'Ты занимался английским сегодня, {}?'
+MSG = 'Do you studied English today, {}?'
 
 
 @dp.message_handler(commands=['start'])
@@ -18,7 +19,7 @@ async def start_handler(message: types.Message):
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id=} {user_full_name=} {time.asctime()=}')
 
-    await message.reply(f'Привет, {user_full_name}')
+    await message.reply(f'Hello, {user_full_name}')
 
     for i in range(7):
         time.sleep(60*60*24)
